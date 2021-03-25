479d2a7699973ec7ae6730ebd6342dc576723d4b
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-03-25 14:20:44 +0530

    Listening events

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 const apiKey = `AIzaSyB7cwx0hAbw1KtZEMRTitDbdv1biUALGcI`;
 const searchId = `c08585697f416761f`;
 
+// Variables intialisation
+const homePageInput = document.getElementById("input");
+const homePageSearchBarBtn = document.querySelector(".search-bar-btn");
+const searchPageInput = document.getElementById("searchPage-input");
+
+// Searching for response
+
+const searchForResponse = () => {
+  console.log("searching response");
+};
+
+// Listening Events
+homePageSearchBarBtn.addEventListener("click", searchForResponse);
+homePageInput.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    searchForResponse();
+  }
+});
+searchPageInput.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    searchForResponse();
+  }
+});
